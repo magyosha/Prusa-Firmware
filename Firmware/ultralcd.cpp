30365883dd190eb1f7a76018371b491bd98d76c3
sauyri <57909519+sauyri@users.noreply.github.com>
2020-05-17 12:50:13 +0200

    Update ultralcd.cpp

--- Firmware/ultralcd.cpp
+++ Firmware/ultralcd.cpp
@@ -4674,7 +4674,7 @@ void lcd_calibrate_pinda() {
 }
 
 void set_bear() {
-     bearCalibration == eeprom_read_byte((unsigned char *)EEPROM_BEARMODE);
+    bearCalibration = eeprom_read_byte((unsigned char *)EEPROM_BEARMODE);
     if(bearCalibration == 1) bearCalibration = 0;
     else bearCalibration = 1;
     eeprom_update_byte((unsigned char *)EEPROM_BEARMODE, bearCalibration);
