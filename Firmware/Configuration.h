67ca566b8fb51ca210404dd1b50349e9ee51ff9e
sauyri <57909519+sauyri@users.noreply.github.com>
2020-05-18 06:29:50 +0200

    Update Configuration.h

--- Firmware/Configuration.h
+++ Firmware/Configuration.h
@@ -16,7 +16,7 @@ extern uint16_t nPrinterType;
 extern PGM_P sPrinterName;
 
 // Firmware version
-#define FW_VERSION "3.9.0-RC7"
+#define FW_VERSION "3.9.0-RC8"
 #define FW_COMMIT_NR   3401
 // FW_VERSION_UNKNOWN means this is an unofficial build.
 // The firmware should only be checked into github with this symbol.
